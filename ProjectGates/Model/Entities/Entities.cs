57f9ca134d60ee138d94a26d995650f6fa64d773
Maciej Pietrek <maciej.pietrek@gmail.com>
2019-02-06 20:40:15 +0100

    not cool #1

---
+++ ProjectGates/Model/Entities/Entities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGates.Model.Entities
+{
+    interface IEntity
+    {
+
+    }
+
+    class ActiveEntity IEntity
+    {
+
+    }
+
+    class PassiveEntity
+    {
+
+    }
+
+    class EphemeralEntity
+    {
+
+    }
+}
